92ca17881315edf36311738cad7603a11d1e5e8f
romainx <romainx@users.noreply.github.com>
2021-05-21 21:00:13 +0200

    Trigger github action

--- tests/test_job.py
+++ tests/test_job.py
@@ -1,6 +1,12 @@
+import logging
+
+
+logger = logging.getLogger(__name__)
+
 def test_secured_server(container, http_client):
     """Notebook server should eventually request user login."""
     container.run()
     resp = http_client.get('http://localhost:8888')
     resp.raise_for_status()
     assert 'login_submit' in resp.text
+    logger.info("All good !")
